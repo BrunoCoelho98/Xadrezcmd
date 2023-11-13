f7900c993927754fa0fe5f4328aaccbebcb328e7
Bruno <br_coelho98@hotmail.com>
2023-11-13 18:22:29 -0300

    Adicionada as primeiras pecas

---
+++ xadrez/jogodexadrez/Rei.cs
@@ -0,0 +1,18 @@
+﻿using tabuleiro;
+
+namespace jogodexadrez
+{
+    internal class Rei : Peca
+    {
+
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+     
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
--- xadrez/tabuleiro/Peca.cs
+++ xadrez/tabuleiro/Peca.cs
@@ -3,18 +3,18 @@ namespace tabuleiro
 {
     internal class Peca
     {
-        public Posicao posicao { get; set; }
+        public Posicao? posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qtdMovimentos {  get; protected set; }
         public Tabuleiro tab {  get; protected set; }
 
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
             this.cor = cor;
             this.qtdMovimentos = 0;
             this.tab = tab;
+            this.posicao = null;
         }
     }
 }
