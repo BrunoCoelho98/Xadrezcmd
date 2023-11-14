1e875815cd853c53983c5e0907d32c4fd1908d17
Bruno <br_coelho98@hotmail.com>
2023-11-13 21:51:44 -0300

    Adicionado posicoes conforme tabuleiro

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -9,21 +9,22 @@ namespace xadez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+
+            PosicaoXadrez pos = new PosicaoXadrez('a',1);
+            /*
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-                Tela.imprimirtabuleiro(tab);
-            } 
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Tela.imprimirtabuleiro(tab);
+            */
+
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
+           
             Console.ReadLine();
         }
     }
