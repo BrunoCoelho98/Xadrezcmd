81a3f59fcaaa940720ccee62939a153945ae66ab
Bruno <br_coelho98@hotmail.com>
2023-11-13 22:18:24 -0300

    Melhorando impressao do tabuleiro

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -10,20 +10,27 @@ namespace xadez
         static void Main(string[] args)
         {
 
-            PosicaoXadrez pos = new PosicaoXadrez('a',1);
-            /*
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            // PosicaoXadrez pos = new PosicaoXadrez('a',1);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 2));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            Tela.imprimirtabuleiro(tab);
-            */
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(4, 4));
 
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
+                Tela.imprimirtabuleiro(tab);
+            }
+            catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
+            
+
+            //Console.WriteLine(pos);
+            //Console.WriteLine(pos.toPosicao());
            
             Console.ReadLine();
         }
